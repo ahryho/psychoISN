fa44264d6a5ccdfbd98fc136caa7b0931f94ddef
Anastasiia Hryhorzhevska <anastasiia_hry@psych.mpg.de>
2022-10-07 18:14:23 +0200

    data preparation

--- code/01_prepare_input_data.R
+++ code/01_prepare_input_data.R
@@ -1,11 +1,20 @@
 # 1. Transform DNAm and SNPs data: individuals are in the rows, features are in the columns
 
-# Load libraries
+## Load libraries
 
 library(data.table)
 
+## Load data
 
-# The data will be stored in /binder/mgp/datasets/2020_DexStim_Array_Human/isn
+dnam_base_mtrx <- fread("~/bio/code/mpip/dex-stim-human-array/data/integrative/matrixEQTL/methyl_beta_mtrx_veh.csv")
+dnam_dex_mtrx  <- fread("~/bio/code/mpip/dex-stim-human-array/data/integrative/matrixEQTL/methyl_beta_mtrx_dex.csv")
+
+## Transform the data: individuals are in the rows, features are in the columns
+
+dnam_base_mtrx_t <- t(dnam_base_mtrx)
+dnam_dex_mtrx_t  <- t(dnam_dex_mtrx)
+
+### The data will be stored in /binder/mgp/datasets/2020_DexStim_Array_Human/isn
 
 # Load the data from dex-stim-human-array proejct
 
