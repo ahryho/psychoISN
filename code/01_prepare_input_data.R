40998bdc40acbabb8f440d781b0ecf7c8b30ff47
ahryho <anasastiia.hryhorzhevska@gmail.com>
2022-10-07 17:58:45 +0200

    data preparation started

---
+++ code/01_prepare_input_data.R
@@ -0,0 +1,12 @@
+# 1. Transform DNAm and SNPs data: individuals are in the rows, features are in the columns
+
+# Load libraries
+
+library(data.table)
+
+
+# The data will be stored in /binder/mgp/datasets/2020_DexStim_Array_Human/isn
+
+# Load the data from dex-stim-human-array proejct
+
+# 2. Correct for covariates
