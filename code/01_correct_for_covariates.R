c19d0a2494a5be493601f0cf702fee10f1e4541a
ahryho <anasastiia.hryhorzhevska@gmail.com>
2022-10-10 17:24:33 +0200

    import data with arrow package

--- code/01_correct_for_covariates.R
+++ code/01_correct_for_covariates.R
@@ -19,7 +19,7 @@ library(doParallel)
 
 treatment   <- "veh"
 
-mtrx_fn     <- paste0("/binder/mgp/workspace//2020_DexStim_Array_Human/dex-stim-human-array/data/integrative/matrixEQTL/methyl_beta_mtrx_", treatment, ".csv")
+mtrx_fn     <- paste0("/binder/mgp/workspace/2020_DexStim_Array_Human/dex-stim-human-array/data/integrative/matrixEQTL/methyl_beta_mtrx_", treatment, ".csv")
 pheno_fn    <- "/binder/mgp/workspace/2020_DexStim_Array_Human/dex-stim-human-array/data/pheno/pheno_full_for_kimono.csv"
 out_mtrx_fn <- "/binder/mgp/workspace/2020_DexStim_Array_Human/isn/input/dnam/methyl_beta_mtrx_corrected_for_cov"
 
