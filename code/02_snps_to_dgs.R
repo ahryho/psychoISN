61d5751cfc5120e1ef2c0df58ed44669c809692a
ahryho <anasastiia.hryhorzhevska@gmail.com>
2022-10-14 20:21:26 +0200

    check the missing geno data

--- code/02_snps_to_dgs.R
+++ code/02_snps_to_dgs.R
@@ -2,8 +2,8 @@ library(SNPRelate)
 library(SmCCNet)
 library(data.table)
 
-
 snp_mpip <- fread("../../../dex-stim-human-array/data/integrative/matrixEQTL/snp_mtrx.csv")
+
 setwd("/binder/mgp/workspace/2020_DexStim_Array_Human/dex-stim-human-isns/input/snps")
 
 # 1. Define the names 
@@ -18,7 +18,7 @@ snp_vcg_fn <- "dex_geno_imputed.vcf"
 
 snpgdsBED2GDS(bed.fn = snp_bed_fn, fam.fn = snp_fam_fn, bim.fn = snp_bim_fn, "gds/dex_geno_imputed.gds")
 
-snpgdsVCF2GDS(vcf.fn = snp_vcg_fn, "gds/dex_geno_imputed_from_vcf.gds")
+# snpgdsVCF2GDS(vcf.fn = snp_vcg_fn, "gds/dex_geno_imputed_original.gds")
 
 # 3. Explore transformed object
 
@@ -36,6 +36,9 @@ rownames(snp_mtrx)   <- geno_obj$sample.id
 
 head(snp_mtrx[, 1:10])
 summary(snp_mtrx[, 1:10])
+
+snp_mtrx <- snp_mtrx[rownames(snp_mtrx) %in% colnames(snp_mpip)[-1],]
+dim(snp_mtrx)
 
 snp_mtrx[is.na(snp_mtrx[, 2]), 2]
 rs <- "rs3131971"
