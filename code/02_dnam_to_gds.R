e48badb483943e5d3abef27d10bdb9aecc439a03
ahryho <anasastiia.hryhorzhevska@gmail.com>
2022-10-17 12:27:23 +0200

    script for trnsformation of dnam beta values to GDS format updated

--- code/02_dnam_to_gds.R
+++ code/02_dnam_to_gds.R
@@ -19,7 +19,7 @@ gds_file <- createfn.gds(gds_fn)
 
 add.gdsn(gds_file, name = "beta_mtrx", val = t(mtrx[, -1]))
 add.gdsn(gds_file, name = "cpg_id", val = mtrx$CpG_ID)
-add.gdsn(gds_file, name = "sample_id", val = colnames(mtrx))
+add.gdsn(gds_file, name = "sample_id", val = colnames(mtrx[, -1]))
 
 show(gds_file)
 
