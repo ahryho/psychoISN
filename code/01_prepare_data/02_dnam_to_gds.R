15209a254a8e42bc85621f8243f4b6198f94eae3
ahryho <anasastiia.hryhorzhevska@gmail.com>
2022-10-26 13:35:15 +0200

    genomic coord added to gds files

--- code/01_prepare_data/02_dnam_to_gds.R
+++ code/01_prepare_data/02_dnam_to_gds.R
@@ -1,11 +1,13 @@
 library(arrow)
 library(gdsfmt)
 
+setwd("/binder/mgp/workspace/2020_DexStim_Array_Human/dex-stim-human-isns/")
+
 # 1. Load corrected for covariates dnam matrices
 
 treatment   <- "dex"
 
-mtrx_fn_prefix <- "/binder/mgp/workspace/2020_DexStim_Array_Human/dex-stim-human-isns/input/dnam/methyl_beta_mtrx_corrected_for_cov"
+mtrx_fn_prefix <- "input/dnam/methyl_beta_mtrx_corrected_for_cov"
 mtrx_fn        <-  paste0(mtrx_fn_prefix, "_", treatment, ".csv")
 
 mtrx <- read_delim_arrow(mtrx_fn,  delim = ";")
@@ -22,5 +24,24 @@ add.gdsn(gds_file, name = "cpg_id", val = mtrx$CpG_ID)
 add.gdsn(gds_file, name = "sample_id", val = colnames(mtrx[, -1]))
 
 show(gds_file)
+
+closefn.gds(gds_file)
+
+# 3. Add chrom and pos into gds file
+
+cpg_loc_fn <- "input/dnam/cpg_locations.csv"
+cpg_loc    <- read_delim_arrow(cpg_loc_fn, delim = ";")
+
+gds_file   <- openfn.gds(gds_fn, readonly = F)
+cpg_ids    <- read.gdsn(index.gdsn(gds_file, "cpg_id"))
+
+order_idx  <- match(cpg_ids, cpg_loc$CpG_ID)
+cpg_loc    <- cpg_loc[order_idx, ]
+
+add.gdsn(gds_file, name = "cpg_chr", val = cpg_loc$chr)
+add.gdsn(gds_file, name = "cpg_pos", val = cpg_loc$pos)
+
+print("Check GDS file: ", quote = F)
+gds_file
 
 closefn.gds(gds_file)
