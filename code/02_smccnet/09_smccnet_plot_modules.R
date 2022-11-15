1c518852bdd85e6d5c83961c8d5e7d1eccd47a13
ahryho <anasastiia.hryhorzhevska@gmail.com>
2022-11-15 20:25:08 +0100

    change the script nam

--- code/02_smccnet/09_smccnet_plot_modules.R
+++ code/02_smccnet/09_smccnet_plot_modules.R
@@ -1,12 +1,7 @@
 library(SmCCNet)
 library(gdsfmt)
-library(SNPRelate)
 
 require(data.table)
-
-library(parallel)
-library(foreach)
-library(doParallel)
 
 source("~/kul/dex-stim-human-array-isns/code/00_functions/load_gds_files.R")
 
@@ -16,11 +11,14 @@ setwd("/binder/mgp/workspace/2020_DexStim_Array_Human/dex-stim-human-isns/")
 
 treatment <- "veh"
 cv_k      <- 5
-cv_dir    <- paste0("results/", cv_k, "_fold_cv/chromosomes/")
-chr       <- 22
+chr       <- "all"
+cv_dir    <- paste0("results/", cv_k, "_fold_cv/") # chromosomes/", chr)
 
-dnam_gds_fn <- paste0("input/dnam/mad_filtered/gds/chromosomes/", treatment, "/methyl_beta_mtrx_corrected_for_cov", "_", treatment, "_chr", chr, ".gds")
-snps_gds_fn <- paste0("input/snps/ld_pruned/gds/chromosomes/dex_geno_chr", chr, ".gds")
+# dnam_gds_fn <- paste0("input/dnam/mad_filtered/gds/chromosomes/", treatment, "/methyl_beta_mtrx_corrected_for_cov", "_", treatment, "_chr", chr, ".gds")
+# snps_gds_fn <- paste0("input/snps/ld_pruned/gds/chromosomes/dex_geno_chr", chr, ".gds")
+
+dnam_gds_fn <- paste0("input/dnam/mad_filtered/gds/methyl_beta_mtrx_corrected_for_cov_mad80_filtered_veh.gds")
+snps_gds_fn <- paste0("input/snps/ld_pruned/gds/dex_geno_imputed_maf_ld_pruned_from_gen.gds")
 
 dnam_mtrx <- LoadMethyl(dnam_gds_fn, is_mad = F)
 snps_mtrx <- LoadGenotype(snps_gds_fn, is_ld = F)
@@ -33,14 +31,27 @@ snps_mtrx <- snps_mtrx[pheno_treatmnet$DNA_ID, ]
 
 # 2. Load SmCCNet results 
 
-modules_obj <- readRDS(file = paste0(cv_dir, chr, "/smccnet_omic_modules_chr_", chr, ".rds"))
+modules_obj <- readRDS(file = paste0(cv_dir, "/smccnet_omic_modules_chr_", chr, ".rds"))
 weights     <- modules_obj$weights
 sim_mtrx    <- modules_obj$sim_mtrx
 modules     <- modules_obj$modules
 features    <- modules_obj$non_zero_features
   
-nr_cpgs     <- table(new_features %in% colnames(dnam_mtrx))[2]
+nr_cpgs     <- table(features %in% colnames(dnam_mtrx))[2]
 
+# 3. Load genomic coordintaes
+
+## 3.1. Load CpGs' coordinates
+
+cpg_loc <- LoadMethylCoordinates(dnam_gds_fn)
+
+## 3.1. Load SNP' coordinates
+
+snp_loc <- LoadGenotypeCoordinates(snps_gds_fn)
+
+# 4. Check how many cis / trans
+
+features
 
 # 3. Plot modules 
 
