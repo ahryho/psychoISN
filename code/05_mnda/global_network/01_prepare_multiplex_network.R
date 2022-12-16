a1b15730bad60db5dd7b21f7f6b7f73d63c43b60
ahryho <anasastiia.hryhorzhevska@gmail.com>
2022-12-16 16:13:49 +0100

    prepare input for mnda

--- code/05_mnda/global_network/01_prepare_multiplex_network.R
+++ code/05_mnda/global_network/01_prepare_multiplex_network.R
@@ -2,7 +2,7 @@ library(dplyr)
 library(data.table)
 library(gdsfmt)
 
-source("~/kul/dex-stim-human-array-isns/code/00_functions/load_gds_files.R")
+source("~/kul/dex-stim-human-array-isns/code/00_functions/flatten_matrix.R")
 
 dir_pre  <- "/binder/mgp/workspace/2020_DexStim_Array_Human/dex-stim-human-isns/"
 rslt_dir <- "/binder/mgp/workspace/2020_DexStim_Array_Human/dex-stim-human-isns/results/5_fold_cv/all/"
@@ -13,3 +13,22 @@ dex_global_net <- readRDS(paste0(rslt_dir, treatment, "/networks/smccnet_global_
 
 treatment <- "veh"
 veh_global_net <- readRDS(paste0(rslt_dir, treatment, "/networks/smccnet_global_chr_all.rds"))
+
+## Flatten matrices
+
+dex_flat_df <- flatten_matrix(dex_global_net)
+veh_flat_df <- flatten_matrix(veh_global_net)
+
+flat_full_df     <- full_join(dex_flat_df, veh_flat_df, by = c("row", "column")) %>% 
+  select(V1 = row, V2 = column, weight_veh = weight.x, weight_dex = weight.y) %>%
+  replace(is.na(.), 0) %>% setDT()
+
+flat_inner_df <- flat_full_df[weight_veh & weight_dex != 0] %>% arrange(weight_veh, weight_dex)
+
+
+flat_df <- rbind(flat_full_df[weight_dex == 0 & weight_veh >= 0.95 ], # disappearing
+                 flat_full_df[weight_veh == 0 & weight_dex >= 0.85 ], # appearing
+                 flat_full_df[weight_veh >= 0.2 & weight_dex >= 0.2] # remaining
+                 )
+
+fwrite(flat_df, "mnda/global_network_mnda_input.csv", quote = F, sep = ";")
