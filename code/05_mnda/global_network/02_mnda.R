e95503951db2560b9b2294e438938e7869a38cc8
Anastasiia Hryhorzhevska <anastasiia_hry@psych.mpg.de>
2022-12-18 18:51:56 +0100

    mnda for global networks: distance calculation and analysis

--- code/05_mnda/global_network/02_mnda.R
+++ code/05_mnda/global_network/02_mnda.R
@@ -14,8 +14,8 @@ library(tictoc)
 # out_fn      <- as.character(args[2]) 
 
 rslt_dir    <- "~/bio/code/kul/dex-stim-human-array-isns/results/data/5_fold_cv/all/"
-input_graph <- paste0(rslt_dir, "mnda/global_network_mnda_input.csv")
-out_fn      <- paste0(rslt_dir, "mnda/global_netqork_mnda_distances.rds")
+input_graph <- paste0(rslt_dir, "mnda/global_network_mnda_input_2K.csv")
+out_fn      <- paste0(rslt_dir, "mnda/global_netqork_mnda_distances_2K.rds")
 
 ## Load data
 
@@ -26,8 +26,13 @@ graph_data <- fread(input_graph, sep = ";")
 print("Start creating embedding space ...", quote = F)
 tic("Create embedding space")
 
-embedding_space_list <- mnda_embedding_2layer(graph_data, train.rep = 50, walk.rep = 100,
-                                              embedding.size = 10, random.walk = T, null.perm = TRUE)
+embedding_space_list <- mnda_embedding_2layer(graph_data, train.rep = 100, 
+                                              walk.rep = 100,
+                                              n.steps = 10,
+                                              embedding.size = 10, 
+                                              batch.size = 32,
+                                              epochs = 50,
+                                              random.walk = T, null.perm = TRUE)
 
 toc()
 
@@ -36,12 +41,12 @@ toc()
 print("Start calculation of the node-pair distances and p-values ...", quote = F)
 tic("Calculate the node-pair distances and p-values")
 
-mnda_dist <- mnda_node_detection_2layer(embedding_space_list, p.adjust.method = "none")
+mnda_dist <- mnda_node_detection_2layer(embedding_space_list, p.adjust.method = "BH")
 
 toc()
 
 ## Save results
 
 tic("Save results")
-saveRDS(mnda_dist, out_fn)
+saveRDS(list(embedding_space = embedding_space_list, mnda_dist = mnda_dist), out_fn)
 toc()
