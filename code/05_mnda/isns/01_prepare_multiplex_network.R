021cd7c4d7781ea241e3074dfd20c7ac2032933c
ahryho <anasastiia.hryhorzhevska@gmail.com>
2022-12-19 11:42:21 +0100

    code for preparing mnda input for isns

---
+++ code/05_mnda/isns/01_prepare_multiplex_network.R
@@ -0,0 +1,56 @@
+library(dplyr)
+library(data.table)
+library(gdsfmt)
+
+source("~/kul/dex-stim-human-array-isns/code/00_functions/flatten_matrix.R")
+
+dir_pre  <- "/binder/mgp/workspace/2020_DexStim_Array_Human/dex-stim-human-isns/"
+rslt_dir <- "/binder/mgp/workspace/2020_DexStim_Array_Human/dex-stim-human-isns/results/5_fold_cv/all/"
+setwd(rslt_dir)
+
+treatment       <- "dex"
+dex_global_net  <- readRDS(paste0(rslt_dir, treatment, "/networks/smccnet_global_chr_all.rds"))
+
+treatment       <- "veh"
+veh_global_net  <- readRDS(paste0(rslt_dir, treatment, "/networks/smccnet_global_chr_all.rds"))
+
+## Flatten global atrices
+
+dex_flat_df <- flatten_matrix(dex_global_net)
+veh_flat_df <- flatten_matrix(veh_global_net)
+
+flat_full_df     <- full_join(dex_flat_df, veh_flat_df, by = c("row", "column")) %>% 
+  select(V1 = row, V2 = column, weight_veh = weight.x, weight_dex = weight.y) %>%
+  replace(is.na(.), 0) %>% setDT()
+
+# flat_inner_df <- flat_full_df[weight_veh & weight_dex != 0] %>% arrange(weight_veh, weight_dex)
+
+flat_df <- rbind(flat_full_df[weight_dex == 0 & weight_veh >= 0.9 ], # disappearing
+                 flat_full_df[weight_veh == 0 & weight_dex >= 0.8 ], # appearing
+                 flat_full_df[weight_veh >= 0.2 & weight_dex >= 0.2] # remaining
+                 )
+
+# fwrite(flat_df, "mnda/global_network_mnda_input.csv", quote = F, sep = ";")
+
+## Prepare ISNs' matrices for MNDA 
+
+associations_of_interest <- flat_df[, 1:2]
+
+### Load a ISNs' filenames: dex and veh have the same names, the difference is in the folder name
+isns_fn_lst <- list.files(paste0("dex/networks"), pattern = "individual", full.names = T)
+
+lapply(isns_fn_lst, function(fn){
+  dex_isn  <- readRDS(paste0(rslt_dir, "dex/networks/smccnet_global_chr_all.rds"))
+  veh_isn  <- readRDS(paste0(rslt_dir, "veh/networks/smccnet_global_chr_all.rds"))
+  
+  ## Flatten global atrices
+  
+  dex_flat_df <- flatten_matrix(dex_global_net)
+  veh_flat_df <- flatten_matrix(veh_global_net)
+  
+  flat_full_df     <- full_join(dex_flat_df, veh_flat_df, by = c("row", "column")) %>% 
+    select(V1 = row, V2 = column, weight_veh = weight.x, weight_dex = weight.y) %>%
+    replace(is.na(.), 0) %>% setDT()
+  
+  isn_mnda_input <- inner_join(associations_of_interest, flat_full_df)
+})
