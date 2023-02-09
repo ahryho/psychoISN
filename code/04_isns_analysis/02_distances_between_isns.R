5e8e658c76ba062ccbb0407954a94be058a63fc1
ahryho <anasastiia.hryhorzhevska@gmail.com>
2023-02-09 13:21:31 +0100

    adjusted for smccnet results

--- code/04_isns_analysis/02_distances_between_isns.R
+++ code/04_isns_analysis/02_distances_between_isns.R
@@ -13,7 +13,7 @@ treatment   <- as.character(args[1])
 rslt_dir    <- as.character(args[2]) 
 
 # rslt_dir <- "/binder/mgp/workspace/2020_DexStim_Array_Human/dex-stim-human-isns/results/5_fold_cv/all/"
-# treatment <- "dex"
+# treatment <- "veh"
 setwd(rslt_dir)
 
 # # 1. Load global network
@@ -38,12 +38,17 @@ setwd(rslt_dir)
 
 # 2. Load ISNs
 
-isns_fn_lst <- list.files(paste0(treatment, "/networks_pearson_cor"), pattern = "individual", full.names = T)
+isns_fn_lst <- list.files(paste0(treatment, "/networks"), pattern = "individual", full.names = T)
 isns_lst    <- lapply(isns_fn_lst, readRDS)
 
-isns_membership_df <- lapply(isns_lst, function(net_obj) data.frame(netID = net_obj[[1]], treatment = treatment)) %>% 
-  bind_rows()
-isns_graph_lst     <- lapply(isns_lst, function(net_obj) as.matrix(net_obj[[2]]))
+isns_membership_df <- lapply(isns_fn_lst, function(fn) {
+  sample_id <- gsub(".*individual_(.+)_chr_all.rds", "\\1", fn) })
+
+isns_graph_lst     <- isns_lst
+
+# isns_membership_df <- lapply(isns_lst, function(net_obj) data.frame(netID = net_obj[[1]], treatment = treatment)) %>% 
+#   bind_rows()
+# isns_graph_lst     <- lapply(isns_lst, function(net_obj) as.matrix(net_obj[[2]]))
 
 rm(isns_lst)
 gc()
@@ -51,10 +56,10 @@ gc()
 # 3. Calculate distances
 
 isns_dist_mtrx  <- euclidean_dist(G = isns_graph_lst, meth = "euclidean")
-colnames(isns_dist_mtrx) <- isns_membership_df$netID
+colnames(isns_dist_mtrx) <- isns_membership_df #$netID
 
 # 4. Save results
 
 fwrite(as.data.table(isns_dist_mtrx, keep.rownames = F), 
-       paste0(rslt_dir, treatment, "/pearson_isns_euclidean_dist_mtrx.csv"),
+       paste0(rslt_dir, treatment, "/smccnet_isns_euclidean_dist_mtrx.csv"),
        quote = F, sep = ";", row.names = F, col.names = T)
